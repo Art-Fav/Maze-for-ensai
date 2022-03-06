200c9624a897652a0e77fb579cbc2652c601420e
Art-Fav <64140585+Art-Fav@users.noreply.github.com>
2022-03-06 13:16:30 +0100

    changes main

--- src/main.py
+++ src/main.py
@@ -1,83 +1,88 @@
+import time
+
 import pygame
+from pygame.locals import (
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
 
 from maze import Maze
 from Q_learning import Q_learning
 
 import parametrage as param
 
+# Initialisation du jeu
 maze = Maze()
 maze.mazing(reward_table=param.reward_table
             , obstacles=param.obstacles
             , bonus=param.bonus)
 
+# Initialisation de la Q_table
 Q_table = Q_learning.initialisation()
 
-# Import pygame.locals for easier access to key coordinates
-# Updated to conform to flake8 and black standards
-from pygame.locals import (
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    K_ESCAPE,
-    KEYDOWN,
-    QUIT,
-)
+# Paramètres d'affichage
+color_table = param.color_table
 
 # Initialize pygame
 pygame.init()
 
-# Define constants for the screen width and height
+# Taille de l'affichage
 SCREEN_WIDTH = 613
 SCREEN_HEIGHT = 613
 
-# Create the screen object
-# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
+# Création de l'affichage
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-# Fill the screen with white
+# Fond noir
 screen.fill((0, 0, 0))
 
-# Variable to keep the main loop running
+# Affichage du jeu
+mat = maze.get_state()
+for i in range(12):
+    for j in range(12):
+        h = 50*i + (i+1)
+        w = 50*j + (j+1)
+        color = color_table[mat[i][j]]
+        surf = pygame.Surface((50, 50))
+        surf.fill(color)
+        rect = surf.get_rect()
+        screen.blit(surf, (w,h))
+        pygame.display.flip()
+
+# Epoque et état de l'apprentissage
 epoch = 0
 state = 0
 
-# Main loop
-while epoch < 1000:
-    # Look at every event in the queue
+# Tant que l'apprentissage n'est pas fini
+while epoch < 10000:
+    t = time.time()
+    
     for event in pygame.event.get():
-        # Did the user hit a key?
         if event.type == KEYDOWN:
-            # Was it the Escape key? If so, stop the loop.
             if event.key == K_ESCAPE:
                 running = False
-        # Did the user click the window close button? If so, stop the loop.
         elif event.type == QUIT:
             running = False
 
-    # Affichage du jeu
-    color_table = param.color_table
-    mat = maze.get_state()
-    for i in range(12):
-        for j in range(12):
-            h = 50*i + (i+1)
-            w = 50*j + (j+1)
-            color = color_table[mat[i][j]]
-            surf = pygame.Surface((50, 50))
-            surf.fill(color)
-            rect = surf.get_rect()
-            screen.blit(surf, (w,h))
-            pygame.display.flip()
-
     if maze.running:
         command = Q_learning.next(Q_table=Q_table
-                                    , state=state)
+                                    , state=state
+                                    , epsilon=0.9)
         reward = maze.move(command=command)
         Q_table = Q_learning.update(Q_table=Q_table
                                     , state=state
                                     , command=command
                                     , reward=reward)      
         state += 1
+        h = 50*maze.row + (maze.row+1)
+        w = 50*maze.col + (maze.col+1)
+        color = color_table[mat[maze.row][maze.col]]
+        surf = pygame.Surface((50, 50))
+        surf.fill(color)
+        rect = surf.get_rect()
+        screen.blit(surf, (w,h))
+        pygame.display.flip()
 
     else:
         maze.mazing(reward_table=param.reward_table
@@ -85,3 +90,19 @@ while epoch < 1000:
                     , bonus=param.bonus)
         epoch += 1
         state = 0
+        mat = maze.get_state()
+        
+        for i in range(12):
+            for j in range(12):
+                h = 50*i + (i+1)
+                w = 50*j + (j+1)
+                color = color_table[mat[i][j]]
+                surf = pygame.Surface((50, 50))
+                surf.fill(color)
+                rect = surf.get_rect()
+                screen.blit(surf, (w,h))
+                pygame.display.flip()
+                
+    while time.time()-t < 0.02:
+        pass
+
